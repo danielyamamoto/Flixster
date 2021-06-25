ddd1e082ae664388b7d888e22cb182f5c5a428ad
danielyamamoto <danylarayamamoto@hotmail.com>
2021-06-24 20:48:19 -0500

    new design for port on details movie

--- app/src/main/java/com/example/flixster/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -2,14 +2,19 @@ package com.example.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.flixster.models.Movie;
 
 import org.parceler.Parcels;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
@@ -19,6 +24,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
     // The view objects
     TextView tvTitle, tvOverview;
     RatingBar rbVoteAverage;
+    ImageView ivPoster;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvOverview = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+        ivPoster = (ImageView) findViewById(R.id.ivPoster);
 
         // unwrap the movie passed in via intent, using its simple name as a key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
@@ -39,5 +46,27 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvOverview.setText(movie.getOverview());
         float voteAverage = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteAverage / 2.0f);
+
+        String imageURL;
+        int radius = 0, margin = 0;
+
+        // if the phone is in landscape
+        if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // then imageURL = back drop image
+            imageURL = movie.getBackdropPath();
+            radius = 10;
+            margin = 0;
+        } else {
+            // else imageURL = poster image
+            imageURL = movie.getPosterPath();
+            radius = 50;
+            margin = 5;
+        }
+
+        Glide.with(this)
+                .load(imageURL)
+                .centerCrop()
+                .transform(new RoundedCornersTransformation(radius, margin))
+                .into(ivPoster);
     }
 }
--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -15,7 +15,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.flixster.MovieDetailsActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
